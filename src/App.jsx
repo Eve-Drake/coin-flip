862d2bccc1b5909a4b3f59b2ba3f59a2ea442359
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-02-15 12:40:12 +0000

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from 'react'
+import {GiDoubleDragon, GiReptileTail} from "react-icons/gi";
+
 
 function App() {
   const [yesNo, setYesNo] = useState({})
 
   const getCoinFlip = () => {
+    try{
     fetch('https://yesno.wtf/api')
     .then(res => res.json())
     .then((res) =>{
       setYesNo(res)
     })
+    }
+    catch(err){
+      console.err(err)
+    }
   }
 
   useEffect(()=>{
@@ -16,9 +23,26 @@ function App() {
   },[])
 
   return (
-    <>
-      <button>Flip</button>
-      <p>{yesNo.answer}</p>
+    <>      
+    
+    <button onClick={()=>getCoinFlip()}>
+        <p className='bold'>{yesNo.answer ==='no'? 'Heads' : 'Tails'}</p>
+        <p>Flip Again?</p>
+      </button>
+
+      {(yesNo.answer === 'no' ? 
+        <div>
+          <GiDoubleDragon size={500} alt='heads'/>
+        </div>
+        
+        : 
+        <div>
+          <GiReptileTail size={500} alt='tails'/>
+        </div>
+        )}
+      
+
+
     </>
   )
 }

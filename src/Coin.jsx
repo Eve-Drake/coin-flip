87f376e910d64808e284aa726a92ccc618321ff6
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-02-27 13:09:15 +0000

    Alternate method coin flip coin change

--- src/Coin.jsx
+++ src/Coin.jsx
@@ -16,6 +16,22 @@ const Coin = ({yesNo, getCoinFlip, headsOrTails}) => {
         </div>
         )}  
     </button>
+
+    {/**
+     *  <button onClick={()=>getCoinFlip()}>
+            {(headsOrTails != 'Heads' ? 
+        <div>
+          <img src='https://www.svgrepo.com/show/307453/whale-tail-wild-wildlife-big.svg'/>
+          <h2>Tails</h2>
+        </div>
+        : 
+        <div>
+          <img src='https://www.svgrepo.com/show/484092/head.svg'/>
+          <h2>Heads</h2>
+        </div>
+        )}  
+    </button>
+     */}
     </>
   )
 }
